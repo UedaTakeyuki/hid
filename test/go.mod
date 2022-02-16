2eed8a234ced39c8c7f89236739fe216d545ae90
Takeyuki UEDA <ueda@latelierdueda.com>
2022-02-16 22:13:50 +0900

    add test, hidType and their methods

--- test/go.mod
+++ test/go.mod
@@ -5,7 +5,7 @@ go 1.15
 replace local.packages/hid => ../../hid
 
 require (
-	github.com/UedaTakeyuki/compare v0.0.0-20220216101204-f64525c76fcc // indirect
-	github.com/speps/go-hashids v2.0.0+incompatible // indirect
-	local.packages/hid v0.0.0-00010101000000-000000000000 // indirect
+	github.com/UedaTakeyuki/compare v0.0.0-20220216101204-f64525c76fcc
+	github.com/speps/go-hashids v2.0.0+incompatible
+	local.packages/hid v0.0.0-00010101000000-000000000000
 )
--- test/hid_test.go
+++ test/hid_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+// Test of hid itself
 func Test_01(t *testing.T) {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 
@@ -33,4 +34,47 @@ func Test_01(t *testing.T) {
 		log.Println("i", i)
 	}
 	cp.Compare(t, i[0], int64(1))
+}
+
+// Test for attached member functions of hid
+func Test_02(t *testing.T) {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+
+	salt := string("For hid test")
+	alphabet := string("abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ") // 50 chars, no "l" nor "I"
+	h, err := hid.CreateHID(salt, alphabet, 8)
+
+	var originali, actuali int = 1, 0
+	var originalii, actualii int64 = 1, 0
+	var str string
+
+	str, err = h.Encode(originali)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("str", str)
+	}
+
+	actuali, err = h.Decode(str)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("actuali", actuali)
+	}
+	cp.Compare(t, actuali, originali)
+
+	str, err = h.EncodeInt64(originalii)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("str", str)
+	}
+
+	actualii, err = h.DecodeInt64(str)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("actualii", actualii)
+	}
+	cp.Compare(t, actualii, originalii)
 }
