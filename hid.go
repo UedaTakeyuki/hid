02007b95ad1af881b1e4eecb14e3a091be11062f
Takeyuki UEDA <ueda@latelierdueda.com>
2022-02-16 22:14:33 +0900

    add test, hidType and their methods

--- hid.go
+++ hid.go
@@ -6,8 +6,12 @@
 package hid
 
 import (
+	"log"
+
 	"github.com/speps/go-hashids"
 )
+
+type hidType hashids.HashID
 
 /*
  * CreateHashID
@@ -19,5 +23,47 @@ func CreateHashID(salt string, alphabet string, minlength int) (h *(hashids.Hash
 	hd.Alphabet = alphabet
 	hd.MinLength = minlength
 	h, err = hashids.NewWithData(hd)
+	return
+}
+
+func CreateHID(salt string, alphabet string, minlength int) (h *hidType, err error) {
+	// hashid
+	var hid *(hashids.HashID)
+	hid, err = CreateHashID(salt, alphabet, minlength)
+	if err == nil {
+		h = (*hidType)(hid)
+	}
+	return
+}
+
+/*
+ * Encode
+ */
+func (h *hidType) Encode(i int) (str string, err error) {
+	str, err = (*(hashids.HashID))(h).Encode([]int{i})
+	return
+}
+
+func (h *hidType) EncodeInt64(i int64) (str string, err error) {
+	str, err = (*(hashids.HashID))(h).EncodeInt64([]int64{i})
+	return
+}
+
+func (h *hidType) Decode(str string) (i int, err error) {
+	var res []int
+	res, err = (*(hashids.HashID))(h).DecodeWithError(str)
+	if err == nil {
+		log.Println("res", res)
+		i = res[0]
+	}
+	return
+}
+
+func (h *hidType) DecodeInt64(str string) (i int64, err error) {
+	var res []int64
+	res, err = (*(hashids.HashID))(h).DecodeInt64WithError(str)
+	if err == nil {
+		i = res[0]
+	}
 	return
 }
